dc83534cc939a608344b614b677414119a6e8e90
yyc12345 <yyc12321@outlook.com>
2018-06-11 11:05:18 +0800

    feat(youku,iqiyi) Finish y/i's code
    - Finish youku and iqiyi's core code

--- README.md
+++ README.md
@@ -28,17 +28,54 @@ or
 {% bilibili %} avCode @ section @ width x height {% endbilibili %}
 ```
 
-> avCode is bilibili video's serial number. Just like this: https://www.bilibili.com/video/av **170001**
+avCode is bilibili video's serial number. Just like this: https://www.bilibili.com/video/av **170001**
 
-> avCode should be a number. Don't attach "av" to it.
+avCode should be a number. Don't attach "av" to it.
 
-> section is matched video's sub video's serial number.
+section is matched video's sub video's serial number.
 
-> width x height is player's size.
+width x height is player's size.
 
-> Bilibili supports HTML5 player. It's familar with Linux user.
+Bilibili supports HTML5 player. It's familar with Linux user.
 
 ### Youku
 
+```
+{% youku %} videoCode {% endyouku %}
+
+or
+
+{% youku %} videoCode @ width x height {% endyouku %}
+```
+
+videoCode just like this: https://v.youku.com/v_show/id_ **XNDU4MjQ3MzA0** .html
+
+width x height is player's size.
+
+Only Flash player
 
 ### Iqiyi
+
+~~Iqiyi's parameter just like shit!!!~~
+
+```
+{% iqiyi %} parameter1 @ video @ albumId @ tvId @ width x height {% endiqiyi %}
+```
+
+You only can visit Iqiyi's video's Sharing page and copy its HTML to get these parameters. It just like this:
+
+```
+<embed src="http://player.video.qiyi.com/1bba1b2ae1e248fdf316c6925eb4d5a2/0/0/w_19rugndwj9.swf-albumId=6462528409-tvId=6462528409-isPurchase=0-cnId=8" allowFullScreen="true" quality="high" width="480" height="350" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>
+```
+
+For this content:
+
+**1bba1b2ae1e248fdf316c6925eb4d5a2** is parameter1.
+
+**w_19rugndwj9** is video
+
+**6462528409** is albumId
+
+**6462528409** is tvId
+
+~~Iqiyi's parameter just like shit!!!~~
--- index.js
+++ index.js
@@ -26,8 +26,69 @@ module.exports = {
                 }
 
                 return '<div style="position: relative;padding-bottom: 56.25%;padding-top: 25px;height: 0;">'
-                + '<iframe height="' + height + '" width="' + width + '" src="https://player.bilibili.com/player.html?aid=' + video + '&page=' + section + '" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>'
-                + '</div>';
+                    + '<iframe height="' + height + '" width="' + width + '" src="https://player.bilibili.com/player.html?aid=' + video + '&page=' + section + '" scrolling="no" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe>'
+                    + '</div>';
+            }
+        },
+        youku: {
+            process: function (block) {
+                var replaced = block.body.trim();
+                var video = replaced;
+                var width = 544;
+                var height = 415;
+
+                if (replaced.indexOf('@') > 0) {
+                    var pair = replaced.split('@');
+                    video = pair[0].trim();
+
+                    var size = pair[1].split('x');
+                    width = size[0].trim();
+                    height = size[1].trim();
+                }
+
+                var url = "https://v.youku.com/v_show/id_" + video + ".html/?br";
+
+                if (this.generator != "website") {
+                    return '<p><a href="' + url + '">Video link</a></p>';
+                }
+
+                return '<div style="position: relative;padding-bottom: 56.25%;padding-top: 25px;height: 0;">'
+                    + '<embed src=\'http://player.youku.com/player.php/sid/' + video + '/v.swf\' allowFullScreen=\'true\' quality=\'high\' width=\'' + width + '\' height=\'' + height + '\' align=\'middle\' allowScriptAccess=\'always\' type=\'application/x-shockwave-flash\'></embed>'
+                    + '</div>';
+            }
+        },
+        iqiqyi: {
+            process: function (block) {
+                var replaced = block.body.trim();
+                var video = replaced;
+                var width = 544;
+                var height = 415;
+
+                var parameter1 = '0';
+                var albumId = '0';
+                var tvId = '0';
+
+                if (replaced.indexOf('@') > 0) {
+                    var pair = replaced.split('@');
+                    parameter1 = pair[0].trim();
+                    video = pair[1].trim();
+                    albumId= pair[2].trim();
+tvId = pair[3].trim();
+
+                    var size = pair[4].split('x');
+                    width = size[0].trim();
+                    height = size[1].trim();
+                }
+
+                var url = "http://www.iqiyi.com/"+video+".html/?br";
+
+                if (this.generator != "website") {
+                    return '<p><a href="' + url + '">Video link</a></p>';
+                }
+
+                return '<div style="position: relative;padding-bottom: 56.25%;padding-top: 25px;height: 0;">'
+                    + '<embed src="http://player.video.qiyi.com/'+parameter1+'/0/0/'+video+'.swf-albumId='+albumId+'-tvId='+tvId+'-isPurchase=0-cnId=8" allowFullScreen="true" quality="high" width="'+width+'" height="'+height+'" align="middle" allowScriptAccess="always" type="application/x-shockwave-flash"></embed>'
+                    + '</div>';
             }
         }
     }
